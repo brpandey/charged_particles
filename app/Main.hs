fd186a5b82cae1c61a62b537503e7d61b2ed6135
Bibek Pandey <bibekp@gmail.com>
2025-08-28 10:48:05 -0700

    Added cmd line # balls option, max being 8

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,26 @@
 import Ball ()
 import Graphics.Gloss
+import System.Environment (getArgs)
 import System.Random
+import Text.Read (readMaybe)
 import Vec (Vec2 (..))
 import World as W
+
+maxBalls :: Int
+maxBalls = 8
 
 -- Main
 main :: IO ()
 main = do
+  args <- getArgs
+  count <- case args of
+    [a] -> case readMaybe a of -- convert single arg into int
+      Just n -> return (min n maxBalls)
+      Nothing -> return maxBalls
+    _ -> return maxBalls
+
   gen <- getStdGen
-  let world = setup Vec2 8 gen
+  let world = setup Vec2 count gen
   simulate window bgColor fps world W.render W.update
   where
     window = InWindow "Charged Patricles: Attract and Repel" (width, height) (100, 100)
